b966592ca866bad2bc31ce4cb7fabfe31501a13e
Naba Kumar Das <nabad600@gmail.com>
2022-09-27 21:57:30 +0530

    Update ubuntu-installation1.sh

--- ubuntu-installation1.sh
+++ ubuntu-installation1.sh
@@ -2,7 +2,7 @@
 #Set up the required package
 echo "Running apt update, installing dependencies"
 sudo apt update
-pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io docker-compose ffmpeg libopencv-dev libopencv*'
+pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io docker-compose ffmpeg'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
 fi
