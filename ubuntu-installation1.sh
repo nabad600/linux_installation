d7b510ab56245014cf9800c2a2998995729d9a19
Naba Kumar Das <nabad600@gmail.com>
2022-09-27 21:56:59 +0530

    Create ubuntu-installation1.sh

---
+++ ubuntu-installation1.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#Set up the required package
+echo "Running apt update, installing dependencies"
+sudo apt update
+pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io docker-compose ffmpeg libopencv-dev libopencv*'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+  sudo apt-get install -y $pkgs
+fi
+pkgs='deck'
+version='4.0.0'
+arch='amd64'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+  echo "Downloading DECK ..."
+  wget https://github.com/deck-app/stable-releases/releases/download/v4.0.0/DECK-$version-linux-$arch.deb
+  sudo dpkg -i DECK-$version-linux-$arch.deb
+  sudo ln -s /opt/DECK/libffmpeg.so /usr/lib/libffmpeg.so
+  git clone --single-branch --branch deck-v4 https://github.com/sfx101/deck.git ~/.deck
+fi
+export PATH=/home/$USER/bin:$PATH
+export DOCKER_HOST=unix:///run/user/$USER/docker.sock
+#List the versions available in your repo
+apt-cache madison docker-ce
+sudo usermod -aG docker $USER
+
+echo "Staring docker";
+sudo loginctl enable-linger $(whoami)
+echo "sudo chmod 666 /var/run/docker.sock";
+sudo chmod 666 /var/run/docker.sock
+compose=$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')
+sudo curl -L "https://github.com/docker/compose/releases/download/$compose/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo rm -rf override.conf
+sudo setcap 'cap_net_bind_service=+eip' /opt/DECK/deck
+sudo ldconfig
+# clear
+# neofetch
+echo "Reloading systemd manager configuration ...";
+sudo systemctl daemon-reload
+sudo systemctl restart docker.service
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
+echo "Installation has finished";
